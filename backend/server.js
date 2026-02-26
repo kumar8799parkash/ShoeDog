acba44c3d9b96e58854373624ea8126031c72a04
kumar8799parkash <kumar8799parkash@gmail.com>
2026-02-26 08:49:39 +0530

    DBSetup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './src/config/db.js';
+import Category from './src/models/CategoryModel.js';
 
 dotenv.config();
 connectDB();
@@ -9,11 +10,31 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 
-app.get('/' ,(req , res)=>{
+app.get('/', (req, res) => {
     res.send('Hello world this is parker')
 })
 
+app.get('/test-category', async (req, res) => {
 
-app.listen(PORT , ()=>{
+    try {
+        const newCategory = new Category({
+            title: 'Men',
+            slug: 'men',
+            imageURL: 'https://pub-8cdcb94d090249c3aa8a0329e70ef215.r2.dev/categories/men.webp',
+            description: 'Boots combine rugged durability with timeless style, making them perfect for both outdoor adventures and everyday wear. Crafted from premium materials with sturdy soles and comfortable lining, they provide excellent support and long-lasting performance. From casual outfits to semi-formal looks, men’s boots add confidence, strength, and versatility to any wardrobe.'
+        })
+
+        newCategory.save();
+
+        console.log("new category saved successfully");
+        res.json(newCategory);
+    }catch(err){
+        console.error(err);
+        res.send("new category FAILED save");
+    }
+    
+})
+
+app.listen(PORT, () => {
     console.log(`app is listening on ${PORT}`);
 })
--- backend/src/models/productModel.js
+++ backend/src/models/productModel.js
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
-
 const ProductSchema = new mongoose.Schema({
+    title : {type:String , required : true},
+    description : {type : String},
+    slug : {type : String , required : true , unique:true},
+    images : {type : [String] ,validate : [arr => arr.length > 0, "At least one image required"] }, //[ validationFunction , errorMessage ]
+    price : {type : Number , required : true , min : 0},
+    category : {type : mongoose.Schema.Types.ObjectId , ref : 'Category' , required : true},
+    tags : [String],
+} , {timestamps : true})
 
-})
+const Product = mongoose.model('Product' , ProductSchema);
+export default Product;
